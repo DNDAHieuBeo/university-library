e6e5b38434806099736376b74267dfdb5d185978
Hieu Le <mac@Hieu-Le.local>
2025-03-15 17:24:53 +0700

    feat:implement qstash workflows for users onboard

--- lib/workflow.ts
+++ lib/workflow.ts
@@ -1,7 +1,36 @@
 import { Client as WorkflowClient } from "@upstash/workflow";
+import { Client as QstashClient, resend } from "@upstash/qstash";
 import config from "@/lib/config";
+
 
 export const workflowClient = new WorkflowClient({
   baseUrl: config.env.upstash.qstashUrl,
   token: config.env.upstash.qstashToken,
 });
+
+const qstashclient = new QstashClient({
+  token: config.env.upstash.qstashToken,
+});
+
+export const sendEmail = async ({
+  email,
+  subject,
+  message,
+}: {
+  email: string;
+  subject: string;
+  message: string;
+}) => {
+  await qstashclient.publishJSON({
+    api: {
+      name: "email",
+      provider: resend({ token: config.env.resendToken }),
+    },
+    body: {
+      from: "Hieu Beo <hieubeo.fun>",
+      to: [email],
+      subject,
+      html: message,
+    },
+  });
+};
